6d99cf52bd9046a91e2b26953ae97b20c12ba2cc
BouBou <69978359+BouBouw@users.noreply.github.com>
2024-02-20 14:40:04 +0100

    traject map

--- resources/js/Pages/Trajects/Layouts/MapsLayout.jsx
+++ resources/js/Pages/Trajects/Layouts/MapsLayout.jsx
@@ -1,6 +1,58 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function MapsLayout() {
+    const url = new URLSearchParams(document.location.search);
+    const uuid = url.get("uuid");
+
+    const [ map, setMap ] = useState(null);
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/api/roads/${uuid}`).then(async (response) => {
+            const data = response.data;
+            
+            const initMap = () => {
+                const directionsService = new window.google.maps.DirectionsService();
+                const directionsRenderer = new window.google.maps.DirectionsRenderer();
+
+                const mapOptions = {
+                    center: { lat: 0, lng: 0 },
+                    zoom: 6,
+                };
+
+                const newMap = new window.google.maps.Map(document.getElementById('map'), mapOptions);
+                directionsRenderer.setMap(newMap);
+
+                const request = {
+                    origin: data.ville_depart,
+                    destination: data.ville_arriver,
+                    travelMode: 'DRIVING',
+                }
+
+                directionsService.route(request, (response, status) => {
+                    if(status === 'OK') {
+                        directionsRenderer.setDirections(response);
+                    } else {
+                        console.error('Erreur', status);
+                    }
+                });
+
+                setMap(newMap);
+            };
+
+            if(window.google && window.google.maps) {
+                initMap();
+            } else {
+                const script = document.createElement('script');
+                script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBh9i8CNaISR_ZMEg-Yq7uTbmKxuF3pCR0&callback=initMap`;
+                script.defer = true;
+                document.body.appendChild(script);
+                script.onload = initMap;
+            }
+        })
+    }, []);
+
     return(
-        <>
-        </>
+        <div id="map" style={{ height: '400px', width: '100%' }}></div>
     )
-};
+}
--- resources/js/Pages/Trajects/Trajects.jsx
+++ resources/js/Pages/Trajects/Trajects.jsx
@@ -8,7 +8,7 @@ import { StandaloneSearchBox, useJsApiLoader } from '@react-google-maps/api';
 
 import { Head } from '@inertiajs/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faCalendarAlt, faCircle, faInfo, faInfoCircle, faStar, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight, faCalendarAlt, faCar, faCircle, faInfo, faInfoCircle, faStar, faUser } from '@fortawesome/free-solid-svg-icons';
 import MapsLayout from './Layouts/MapsLayout';
 import { useEffect, useRef, useState } from 'react';
 
@@ -54,21 +54,27 @@ export default function Trajects({ auth }) {
       }
 
     useEffect(() => {
-        if(!uuid) return;
-        axios.get(`http://127.0.0.1:8000/api/roads/${uuid}`).then(async (response) => {
-            console.log(response.data)
-            await setData(response.data)
-        })
+        if(!uuid) {
 
-        if(!date) return;
-        axios.post("http://127.0.0.1:8000/api/search", {
-            ville_depart: `${ !departure_city ? '' : departure_city }`,
-            ville_arriver: `${ !arrival_city ? '' : arrival_city }`,
-            date_traject: `${date}`
-        }).then(async (response) => {
-            console.log(response.data);
-            await setRoads(response.data);
-        })
+        } else {
+            axios.get(`http://127.0.0.1:8000/api/roads/${uuid}`).then(async (response) => {
+                console.log(response.data)
+                await setData(response.data)
+            })
+        }
+
+        if(!date) {
+
+        } else {
+            axios.post("http://127.0.0.1:8000/api/search", {
+                ville_depart: "",
+                ville_arriver: "",
+                date_traject: "2024-03-08"
+            }).then(async (response) => {
+                console.log(response);
+                await setRoads(response.data);
+            })
+        }
     }, [])
 
     return(
@@ -81,6 +87,66 @@ export default function Trajects({ auth }) {
                 <>
                     {!date ?
                         <>
+                        <div className='flex flex-row justify-center items-center p-10' id='main-content'>
+                        <div className='flex justify-between bg-white p-3 rounded-l-xl'>
+                            <div className='order-1'>
+                                <div className='flex flex-row space-x-3 items-center'>
+                                    <FontAwesomeIcon icon={ faCircle } className='text-green-500' />
+                                    { isLoaded ?
+                                        <>
+                                            <StandaloneSearchBox 
+                                              onLoad={ ref => (inputRef.current = ref) }
+                                              onPlacesChanged={ handlePlaceChanged }
+                                            >
+                                              <input type='text' value={ !departure_city ? departure : departure_city } className='form-control rounded-lg text-black placeholder:text-black border border-gray-200' placeholder='Départ' onChange={(e) => { setDeparture(e.target.value) }}>
+                                              </input>
+                                            </StandaloneSearchBox>
+                                        </>
+                                    :
+                                        <>
+                                          none
+                                        </>
+                                    }
+                                </div>
+                            </div>
+                            <div className='ml-5 order-2'>
+                                <div className='flex flex-row space-x-3 items-center'>
+                                    <FontAwesomeIcon icon={ faCircle } className='text-green-500' />
+                                    { isLoaded ?
+                                        <>
+                                          <StandaloneSearchBox 
+                                            onLoad={ ref => (inputRef_1.current = ref) }
+                                            onPlacesChanged={ handlePlaceChanged_1 }
+                                          >
+                                            <input type='text' value={ !arrival_city ? arrival : arrival_city  } className='form-control rounded-lg text-black placeholder:text-black border border-gray-200' placeholder='Arrivé' onChange={(e) => { setArrival(e.target.value) }}></input>
+                                          </StandaloneSearchBox>
+                                        </>
+                                    :
+                                        <>
+                                          none
+                                        </>
+                                    }
+                                </div>
+                            </div>
+                            <div className='ml-5 order-3'>
+                                <div className='flex flex-row space-x-3 items-center'>
+                                  <FontAwesomeIcon icon={ faUser } className='text-green-500' />
+                                  <input type='number' value={ !passengers ? 1 : passengers  } placeholder='0' className='w-14 rounded-lg text-black placeholder:text-black border border-gray-200' />
+                                </div>
+                            </div>
+                            <div className='ml-5 order-4'>
+                              <div className='flex flex-row space-x-3 items-center'>
+                                <FontAwesomeIcon icon={ faCalendarAlt } className='text-green-500' />
+                                <input type='date' value={ date } className='rounded-lg text-black placeholder:text-black border border-gray-200'/>
+                              </div>
+                            </div>
+                        </div>
+                        <button className='text-white font-bold flex items-center justify-center p-5 bg-blue-600 hover:bg-blue-700 rounded-r-xl' onClick={(e) => {
+                            Search();
+                        }}>
+                            Rechercher
+                        </button>
+                    </div>
                             <div className="p-5">
                                 <RoadsList/>
                             </div>
@@ -156,7 +222,7 @@ export default function Trajects({ auth }) {
                                     <>
                                         <div className='grid grid-cols-4 gap-4'>
                                             { roads.map((item, index) => (
-                                                <div className="bg-neutral-100 shadow-md hover:shadow-xl p-5 w-52 rounded-lg">
+                                                <div className="bg-neutral-100 shadow-md hover:shadow-xl p-5 w-52 rounded-lg" key={ index }>
                                                     <div className="flex justify-between">
                                                         <div className="order-1">
                                                             <div className="flex flex-row space-x-2 items-center">
